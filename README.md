38e420a5840403fc77e62bd7605dcd653016ade6
Mary West <mary.west@sparkfun.com>
2017-07-18 17:55:18 -0600

    updating files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# pxt-tmp36
-Makecode package for the TMP36 temperature sensor
+# tmp36
+
+TODO: To use this package, go to https://makecode.microbit.org, click ``Add package`` and search for **tmp36**.
+
+## Usage
+
+The package adds support for the **tmp36** sensor used in the SparkFun Inventor's Kit.
+
+	
+
+### Micro:bit Pins Used 
+see guide
+
+
+
+## Supported targets
+
+* for PXT/microbit
+
+```package
+tmp36=github:sparkfun/pxt-tmp36
+```
--- tmp36.ts
+++ tmp36.ts
@@ -1,11 +1,11 @@
 /**
 * Mary West @ SparkFun Electronics 
-* July 3, 2017
+* June 30, 2017
 * https://github.com/sparkfun/pxt-tmp36
 
 * Development environment specifics:
-* Written in Microsoft Makecode
-* Tested with a SparkFun TMP36 sensor and micro:bit
+* Written in Microsoft makecode
+* Tested with a micro:bit
 *
 * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 * Please review the LICENSE.md file included with this example. If you have any questions 
@@ -15,64 +15,53 @@
 
 
 /**
- * Functions to operate the TMP36 sensor
+ * Functions to operate the tmp36
  */
- 
- enum tmp36Type{
-	 //% blockId = "F" block = "Degress in F"
-	 F=1,
-	 
-	 //% blockId ="C" block = "Degrees in C"
-	 C=2,
-	 
-	 //% blockId="adcval" block = "ADC Value"
-	 adcVal=3,
- }
- 
- 
-
-//% color=#f44242 icon="\u26C8"
-namespace tmp36 {
     
-    // Functions for reading temperature from the TMP36 in degrees C or F
 
-    /**
-    * Reads the number 
-	* R
-    */
-    //% weight=30 blockId="tmp36_temp" block="Get Temperature on pin %pin| in %type"
-    export function temp(pin: AnalogPin, type: tmp36Type): number{
-		let tempADCVal = pins.analogReadPin(pin)
-		switch(type){
-		case tmp36Type.F: return getDegF(tempADCVal)
-		case tmp36Type.C: return getDegC(tempADCVal)
-		case tmp36Type.adcVal: return tempADCVal
-		}
-    }
-
+//% color=#f44242 icon="\uf2ca" block = "tmp36"
+namespace tmp36 {
+  
 	/**
-     * Function used for simulator, actual implementation is in tmp36.cpp
-     */
-    //% shim=tmp36::getDegF
-    function getDegF(tempADCVal: number) {
-        // Fake function for simulator
-        return 0
+    * Reads the value from the analog pin of the users choice 
+	* 
+    */
+    //% weight=30 blockId="tmp26_temp(c)" block="temp(c) on pin %pin"
+    export function tempC(pin:AnalogPin): number {
+        let tc = 0
+		tc=pins.analogReadPin(pin)
+        return tc
     }
 	
-	/**
+
+	
+	
+
+    /**
      * Function used for simulator, actual implementation is in tmp36.cpp
      */
-    //% shim=tmp36::getDegC
-    function getDegC(tempADCVal: number) {
+    //% shim=tmp36::getTempC
+    function getTempC(adcVal: number) {
         // Fake function for simulator
         return 0
     }
+	 /**
+     * Function used for simulator, actual implementation is in tmp36.cpp
+     */
+    //% shim=tmp36::getTempF
+    function getTempF(adcVal: number) {
+        // Fake function for simulator
+        return 0
    
+    /**
+     * Reads the temperature from the one-wire temperature sensor.
+	 * Returns a 4 digit number. value should be divided by 100 to get 
+	 *temperature in hudnreths of a degree centigrade. 
+     */
+    //% weight=10 blockId="weahterbit_soilTemp" block="soil temperature(C)"
+    //% shim=weatherbit::soilTemp
+    export function soilTemperature(): number {
+        // Fake function for simulator
+        return 0
     }
-
-    
-        
-
-    
-
-    
+}
