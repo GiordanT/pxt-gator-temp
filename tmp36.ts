5d7fe122eb9b40e1ec1171a6ac71ba314b8ca77d
rmortens <ryan.j.mortenson@gmail.com>
2017-07-18 19:00:03 -0600

    Fix tmp36 enum issues

--- tmp36.ts
+++ tmp36.ts
@@ -1,11 +1,11 @@
 /**
 * Mary West @ SparkFun Electronics
-* June 30, 2017
+* July 3, 2017
 * https://github.com/sparkfun/pxt-tmp36
 
 * Development environment specifics:
-* Written in Microsoft makecode
-* Tested with a micro:bit
+* Written in Microsoft Makecode
+* Tested with a SparkFun TMP36 sensor and micro:bit
 *
 * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 * Please review the LICENSE.md file included with this example. If you have any questions
@@ -15,39 +15,52 @@
 
 
 /**
- * Functions to operate the tmp36
+ * Functions to operate the TMP36 sensor
  */
 
+ enum tmp36Type{
+	 F=1,
+	 C=2,
+	 adcVal=3,
+ }
 
-//% color=#f44242 icon="\uf2ca" block = "tmp36"
+
+
+//% color=#f44242 icon="\u26C8"
 namespace tmp36 {
 
-   /**
-    * Reads the value from the analog pin of the users choice
-    *
+    // Functions for reading temperature from the TMP36 in degrees C or F
+
+    /**
+    * Reads the number
     */
-    //% weight=30 blockId="tmp26_temp(c)" block="temp(c) on pin %pin"
-    export function tempC(pin:AnalogPin): number {
-        let tc = 0
-        tc=pins.analogReadPin(pin)
-        return tc
+    //% weight=30 blockId="tmp36_temp" block="Get Temperature on pin %pin | in %tmp36Type"
+    export function temp(pin: AnalogPin, type: tmp36Type): number{
+      let tempADCVal = pins.analogReadPin(pin)
+      switch(type){
+        case tmp36Type.F: return getDegF(tempADCVal)
+        case tmp36Type.C: return getDegC(tempADCVal)
+        case tmp36Type.adcVal: return tempADCVal
+        default: return -11111111
+      }
     }
 
-    /**
+	/**
      * Function used for simulator, actual implementation is in tmp36.cpp
      */
-    //% shim=tmp36::getTempC
-    function getTempC(adcVal: number) {
+    //% shim=tmp36::getDegF
+    function getDegF(tempADCVal: number) {
         // Fake function for simulator
         return 0
     }
 
-    /**
+	/**
      * Function used for simulator, actual implementation is in tmp36.cpp
      */
-    //% shim=tmp36::getTempF
-    function getTempF(adcVal: number) {
+    //% shim=tmp36::getDegC
+    function getDegC(tempADCVal: number) {
         // Fake function for simulator
         return 0
     }
-}
+
+    }
