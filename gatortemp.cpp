b0c48fcdf121f64dbf8bbdfd14d9b7dfa355ee54
AndyEngland521 <andrew.england@sparkfun.com>
2018-10-03 14:15:09 -0600

    rebuilding

--- gatortemp.cpp
+++ gatortemp.cpp
@@ -1,11 +1,12 @@
+
 /**
 * Mary West @ SparkFun Electronics
 * Juuly 3, 2017
-* https://github.com/sparkfun/pxt-tmp36
+* https://github.com/sparkfun/pxt-gator-temp
 *
 * Development environment specifics:
 * Written in Microsoft PXT
-* Tested with a SparkFun TMP36 sensor and micro:bit
+* Tested with a SparkFun MCP9700 sensor and micro:bit
 *
 * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 * Please review the LICENSE.md file included with this example. If you have any questions
