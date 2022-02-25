4a1ba21a8a3a03548da9c927249c9bd21482e8f6
GiordanT <giordan.thompson@sparkfun.com>
2022-02-25 14:53:17 -0700

    gatorTemp Test

--- enums.d.ts
+++ enums.d.ts
@@ -1,5 +1,5 @@
 // Auto-generated. Do not edit.
-declare namespace gatorTemp {
+declare namespace gatortemp {
 }
 
 // Auto-generated. Do not edit. Really.
--- shims.d.ts
+++ shims.d.ts
@@ -16,7 +16,7 @@
      * Distributed as-is; no warranty is given.
      */
 
-declare namespace gatorTemp {
+declare namespace gatortemp {
 }
 
 // Auto-generated. Do not edit. Really.
